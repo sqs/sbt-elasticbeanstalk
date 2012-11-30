face0e1f7fea14f69e4183c99f2768a99bf7c3aa
Quinn Slack <qslack@qslack.com>
2012-11-30 12:25:52 -0800

    Show Amazon S3 upload progress

--- core/src/main/scala/com/blendlabsinc/sbtelasticbeanstalk/core/SourceBundleUploader.scala
+++ core/src/main/scala/com/blendlabsinc/sbtelasticbeanstalk/core/SourceBundleUploader.scala
@@ -1,9 +1,10 @@
 package com.blendlabsinc.sbtelasticbeanstalk.core
 
 import com.amazonaws.auth.AWSCredentials
-import com.amazonaws.services.s3.AmazonS3Client
+//import com.amazonaws.services.s3.AmazonS3Client
 import com.amazonaws.services.s3.model._
 import com.amazonaws.services.elasticbeanstalk.model._
+import com.amazonaws.services.s3.transfer._
 import java.io.File
 import java.util.Date
 import java.text.SimpleDateFormat
@@ -13,17 +14,41 @@ class SourceBundleUploader(
   s3BucketName: String,
   awsCredentials: AWSCredentials
 ) {
-  private val s3 = new AmazonS3Client(awsCredentials)
   private val dateFormatter = new SimpleDateFormat("yyMMddHHmmssZ")
 
   // TODO: make async
   def upload(): S3Location = {
     val key = bundleFile.getName + "-" + System.getenv("USER") + "-" + dateFormatter.format(new Date)
-    s3.putObject(new PutObjectRequest(
+
+    val tx = new TransferManager(awsCredentials)
+
+    var upload: Upload = null
+
+    val progressListener = new ProgressListener() {
+      def progressChanged(event: ProgressEvent) {
+        val progress = upload.getProgress
+        def bytesToMB(bytes: Long): Double = (bytes.toDouble / 1024 / 1024)
+        print("\rTransferred: %.1f/%.1f MB (%.1f%%)".format(
+          bytesToMB(progress.getBytesTransfered),
+          bytesToMB(progress.getTotalBytesToTransfer),
+          progress.getPercentTransfered
+        ))
+
+        val code = event.getEventCode
+        if (code == ProgressEvent.COMPLETED_EVENT_CODE || code == ProgressEvent.FAILED_EVENT_CODE) {
+          print("\n")
+        }
+      }
+    }
+
+    val req = new PutObjectRequest(
       s3BucketName,
       key,
       bundleFile
-    ))
+    ).withProgressListener(progressListener)
+
+    upload = tx.upload(req)
+    upload.waitForUploadResult()
     new S3Location(s3BucketName, key)
   }
 }
