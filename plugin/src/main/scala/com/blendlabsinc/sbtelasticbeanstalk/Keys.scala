2e3659d80ade49603b3ba597ffa14ef0eaf8a6c8
Quinn Slack <qslack@qslack.com>
2012-11-30 12:00:36 -0800

    Allow configuration of Elastic Beanstalk app name, env, S3 bucket, AWS region

--- plugin/src/main/scala/com/blendlabsinc/sbtelasticbeanstalk/Keys.scala
+++ plugin/src/main/scala/com/blendlabsinc/sbtelasticbeanstalk/Keys.scala
@@ -1,7 +1,13 @@
 package com.blendlabsinc.sbtelasticbeanstalk
 
-import sbt.TaskKey
+import sbt.{ SettingKey, TaskKey }
 
 object ElasticBeanstalkKeys {
+  val ebS3BucketName = SettingKey[String]("ebS3BucketName", "S3 bucket which should contain uploaded WAR files")
+  val ebAppName = SettingKey[String]("ebAppName", "Elastic Beanstalk application name")
+  val ebEnvironmentName = SettingKey[String]("ebEnvironmentName", "Elastic Beanstalk environment name")
+
+  val ebRegion = SettingKey[String]("ebRegion", "Elastic Beanstalk region (e.g., us-west-1)")
+
   val ebDeploy = TaskKey[Unit]("eb-deploy", "Deploy the application WAR to Elastic Beanstalk")
 }
--- sample-Play2-app/project/Build.scala
+++ sample-Play2-app/project/Build.scala
@@ -2,6 +2,7 @@ import sbt._
 import Keys._
 import play.Project._
 import com.blendlabsinc.sbtelasticbeanstalk.ElasticBeanstalk.elasticBeanstalkSettings
+import com.blendlabsinc.sbtelasticbeanstalk.ElasticBeanstalkKeys._
 import com.github.play2war.plugin._
 
 object ApplicationBuild extends Build {
@@ -11,7 +12,11 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq()
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    Play2WarKeys.servletVersion := "3.0"
+    Play2WarKeys.servletVersion := "3.0",
+    ebS3BucketName := "sbt-elasticbeanstalk-test",
+    ebAppName := "sbt-elasticbeanstalk-sample-Play2-app",
+    ebEnvironmentName := "sbt-eb-sample-Play2",
+    ebRegion := "us-west-1"
   )
     .settings(elasticBeanstalkSettings: _*)
     .settings(Play2WarPlugin.play2WarSettings: _*)
