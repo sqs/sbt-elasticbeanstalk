6c08f194bd4a46624a311f77d62e46160c8b2078
Quinn Slack <qslack@qslack.com>
2012-11-30 09:08:24 -0800

    Use play2-war-plugin to generate WAR in sample Play2 app

--- sample-Play2-app/project/Build.scala
+++ sample-Play2-app/project/Build.scala
@@ -2,6 +2,7 @@ import sbt._
 import Keys._
 import play.Project._
 import com.blendlabsinc.sbtelasticbeanstalk.ElasticBeanstalk.elasticBeanstalkSettings
+import com.github.play2war.plugin._
 
 object ApplicationBuild extends Build {
   val appName         = "sbt-elasticbeanstalk-sample-Play2-app"
@@ -10,6 +11,8 @@ object ApplicationBuild extends Build {
   val appDependencies = Seq()
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-    elasticBeanstalkSettings: _*
+    Play2WarKeys.servletVersion := "3.0"
   )
+    .settings(elasticBeanstalkSettings: _*)
+    .settings(Play2WarPlugin.play2WarSettings: _*)
 }
