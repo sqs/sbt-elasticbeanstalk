934b3f022c4aa3386a9d312b34205597010e5a3b
Quinn Slack <qslack@qslack.com>
2012-12-01 10:33:03 -0800

    Use play2-war-plugin 0.9a-SNAPSHOT (patches it to allow dotfiles in webappResource dir)

--- project/Build.scala
+++ project/Build.scala
@@ -28,8 +28,9 @@ object Build extends Build {
     settings = commonSettings
   ).settings(
     sbtPlugin := true,
+    resolvers += Resolver.url("SQS Ivy", url("http://sqs.github.com/repo"))(Resolver.ivyStylePatterns),
     libraryDependencies ++= Seq(
-      "com.github.play2war" % "play2-war-plugin" % "0.9-SNAPSHOT" % "provided->default(compile)" extra ("scalaVersion" -> "2.9.2", "sbtVersion" -> "0.12")
+      "com.github.play2war" % "play2-war-plugin" % "0.9a-SNAPSHOT" % "provided->default(compile)" extra ("scalaVersion" -> "2.9.2", "sbtVersion" -> "0.12")
     )
   ).dependsOn(sbtElasticBeanstalkCore).aggregate(sbtElasticBeanstalkCore)
 
