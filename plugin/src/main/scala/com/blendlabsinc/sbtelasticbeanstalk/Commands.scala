858067194239a169809b72c9d68e389cb339cab9
Quinn Slack <qslack@qslack.com>
2012-12-04 17:05:19 -0800

    Wait in eb-deploy task deps

--- plugin/src/main/scala/com/blendlabsinc/sbtelasticbeanstalk/Commands.scala
+++ plugin/src/main/scala/com/blendlabsinc/sbtelasticbeanstalk/Commands.scala
@@ -14,8 +14,8 @@ import scala.collection.JavaConversions._
 import scala.collection.JavaConverters._
 
 trait ElasticBeanstalkCommands {
-  val ebDeployTask = (eb.ebSetUpEnvForAppVersion, eb.ebRegion, eb.ebParentEnvironments, state, streams) map {
-    (setUpEnvs, ebRegion, parentEnvs, state, s) => {
+  val ebDeployTask = (eb.ebWait, eb.ebSetUpEnvForAppVersion, eb.ebRegion, eb.ebParentEnvironments, state, streams) map {
+    (waited, setUpEnvs, ebRegion, parentEnvs, state, s) => {
       java.lang.Thread.sleep(5000)
       Project.runTask(eb.ebWait, state)
       val ebClient = AWS.elasticBeanstalkClient(ebRegion)
